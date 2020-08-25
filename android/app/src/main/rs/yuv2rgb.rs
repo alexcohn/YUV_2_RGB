e60a8f987cf9b62886c437b4ec843d487b5c35fb
alexcohn <sasha.cohn@gmail.com>
2020-08-25 11:31:09 +0300

    use JPEG conversion formula
    for camera image, the NTSC ([BT.601](https://en.wikipedia.org/wiki/Rec._601)) conversion formula is not correct. We need the [JPEG conversion](https://en.wikipedia.org/wiki/YCbCr#JPEG_conversion) in full range 0..255 instead.

--- android/app/src/main/rs/yuv2rgb.rs
+++ android/app/src/main/rs/yuv2rgb.rs
@@ -15,11 +15,17 @@ uint32_t Height;
 
 uchar4 __attribute__((kernel)) YUV420toRGB(uint32_t x, uint32_t y)
 {
-    uchar Y = rsGetElementAt_uchar(Yplane, x + y * Yline);
-    uchar V = rsGetElementAt_uchar(Vplane, (x & ~1) + y/2 * UVline);
-    uchar U = rsGetElementAt_uchar(Uplane, (x & ~1) + y/2 * UVline);
-    uchar4 rgb = rsYuvToRGBA_uchar4(Y, U, V);
-    return rgb;
+    short Y = rsGetElementAt_uchar(Yplane, x + y * Yline);
+    short V = rsGetElementAt_uchar(Vplane, (x & ~1) + y/2 * UVline) - 128;
+    short U = rsGetElementAt_uchar(Uplane, (x & ~1) + y/2 * UVline) - 128;
+    // https://en.wikipedia.org/wiki/YCbCr#JPEG_conversion
+    short R = Y + (512           + 1436 * V) / 1024; //             1.402
+    short G = Y + (512 -  352 * U - 731 * V) / 1024; // -0.344136  -0.714136
+    short B = Y + (512 + 1815 * U          ) / 1024; //  1.772
+    if (R < 0) R = 0; else if (R > 255) R = 255;
+    if (G < 0) G = 0; else if (G > 255) G = 255;
+    if (B < 0) B = 0; else if (B > 255) B = 255;
+    return (uchar4){R, G, B, 255};
 }
 
 uchar4 __attribute__((kernel)) YUV420toRGB_180(uint32_t x, uint32_t y)
