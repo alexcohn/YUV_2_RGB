7578c20e01375fa1448d542a0144352b9973c2c8
alexcohn <sasha.cohn@gmail.com>
2020-08-30 23:52:28 +0300

    expose rotation parameter to dart; add compress parameter

--- android/app/src/main/java/tomerblecher/yuvtransform/MainActivity.java
+++ android/app/src/main/java/tomerblecher/yuvtransform/MainActivity.java
@@ -12,6 +12,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.util.Date;
 import java.util.List;
 
@@ -24,8 +25,9 @@ public class MainActivity extends FlutterActivity {
     // Set a name for the method chanel.
     // This name is a key for the Flutter MethodChannel and needs to be equal to the name configured at the dart part
     private static final String CHANNEL = "tomer.blecher.yuv_transform/yuv";
-    private long total_conversion = 0;
-    private long total_jpeg = 0;
+
+    private long total_bitmap = 0;
+    private long total_convert = 0;
     private long total_calls = 0;
     private YuvConverter yuvConverter;
 
@@ -53,56 +55,10 @@ public class MainActivity extends FlutterActivity {
                                     int[] strides = call.argument("strides");
                                     int width = call.argument("width");
                                     int height = call.argument("height");
+                                    boolean compress = call.argument("compress");
+                                    int rotation = call.argument("rotation");
 
-                                    if (total_calls == 0) {
-                                        Log.w("flutter ", "row   strides " + strides[0] + ", " + strides[2] + ", " + strides[4]);
-                                        Log.w("flutter ", "pixel strides " + strides[1] + ", " + strides[3] + ", " + strides[5]);
-                                        Log.w("flutter ", "plane 0 " + bytesList.get(0).length + " " + strides[0] * height);
-                                        Log.w("flutter ", "plane 1 " + bytesList.get(1).length + " " + strides[2] * height/2);
-                                        Log.w("flutter ", "plane 2 " + bytesList.get(2).length + " " + strides[4] * height/2);
-
-                                        long start = new Date().getTime();
-                                        yuvConverter = new YuvConverter(getApplicationContext(), bytesList.get(0).length, bytesList.get(1) == null ? 0 : bytesList.get(1).length, height, width);
-                                        Log.i("flutter ", "yuv_transform init YuvConverter in " + ((new Date().getTime() - start)) + " ms");
-                                    }
-
-                                    total_calls += 1;
-                                    long startTime = new Date().getTime();
-                                    try {
-                                        /*
-                                         * hardcoded assuptions:
-                                         *   stride[0] (yLine) >= width
-                                         *   stride[1] (yPixel) == 1
-                                         *   stride[2] (uLine) >= width
-                                         *   stride[3] (uPixel) == 2
-                                         *   stride[4] (vLine) == uLine
-                                         *   stride[5] (vPixel) == uPixel
-                                         */
-                                        Bitmap bitmapRaw = yuvConverter.YUV420toRGB(bytesList.get(0), bytesList.get(1), bytesList.get(2),
-                                                strides[0], strides[2], height, width, Surface.ROTATION_90);
-
-                                        long conversionTime = new Date().getTime() - startTime;
-                                        total_conversion += conversionTime;
-                                        Log.i("flutter ", "yuv_transform bitmap " + width + "x" + height + " in " + (new Date().getTime() - startTime) + " ms, average "  + total_conversion/total_calls);
-
-                                        ByteArrayOutputStream outputStreamCompressed = new ByteArrayOutputStream();
-                                        bitmapRaw.compress(Bitmap.CompressFormat.JPEG, 60, outputStreamCompressed);
-
-                                        total_jpeg += new Date().getTime() - startTime;
-                                        Log.i("flutter ", "yuv_transform jpeg compression " + (new Date().getTime() - conversionTime - startTime) + " average " + (total_jpeg-total_conversion)/total_calls + " ms");
-
-                                        if (total_calls == 10) {
-                                            FileOutputStream jpg = new FileOutputStream(new File(getCacheDir(), "rotated.jpg"));
-                                            jpg.write(outputStreamCompressed.toByteArray());
-                                            jpg.close();
-                                            Log.i("flutter ", "yuv_transform rotated Jpeg saved to " + new File(getCacheDir(), "rotated.jpg"));
-                                        }
-
-                                        result.success(outputStreamCompressed.toByteArray());
-                                        outputStreamCompressed.close();
-                                    } catch (IOException e) {
-                                        e.printStackTrace();
-                                    }
+                                    result.success(yuvTransform(bytesList, strides, width, height, compress, rotation));
                                     break;
                                 }
                             }
@@ -111,4 +67,66 @@ public class MainActivity extends FlutterActivity {
 
     }
 
+    private byte[] yuvTransform(List<byte[]> bytesList, int[] strides, int width, int height, boolean compress, @YuvConverter.Rotation int rotation) {
+        if (rotation == Surface.ROTATION_90 || rotation == Surface.ROTATION_270) {
+            int _tmp = width;
+            width = height;
+            height = _tmp;
+        }
+        if (total_calls == 0) {
+            long start = new Date().getTime();
+            yuvConverter = new YuvConverter(getApplicationContext(), bytesList.get(0).length, bytesList.get(1) == null ? 0 : bytesList.get(1).length, width, height);
+            Log.i("flutter ", "yuv_transform init YuvConverter in " + ((new Date().getTime() - start)) + " ms");
+        }
+
+        total_calls += 1;
+        long startTime = new Date().getTime();
+        try {
+            /*
+             * hardcoded assuptions:
+             *   stride[0] (yLine) >= width
+             *   stride[1] (yPixel) == 1
+             *   stride[2] (uLine) >= width
+             *   stride[3] (uPixel) == 2
+             *   stride[4] (vLine) == uLine
+             *   stride[5] (vPixel) == uPixel
+             */
+            Bitmap bitmapRaw = yuvConverter.YUV420toRGB(bytesList.get(0), bytesList.get(1), bytesList.get(2),
+                    strides[0], strides[2], width, height, rotation);
+
+            long bitmapTime = new Date().getTime() - startTime;
+            total_bitmap += bitmapTime;
+            Log.i("flutter ", "yuv_transform bitmap " + width + "x" + height + " in " + (new Date().getTime() - startTime) + " ms, average "  + total_bitmap /total_calls);
+
+            if (!compress) {
+                int size = bitmapRaw.getRowBytes() * bitmapRaw.getHeight();
+                ByteBuffer byteBuffer = ByteBuffer.allocate(size);
+                bitmapRaw.copyPixelsToBuffer(byteBuffer);
+
+                total_convert += new Date().getTime() - startTime;
+                Log.i("flutter ", "yuv_transform to byte[] " + (new Date().getTime() - bitmapTime - startTime) + " average " + (total_convert - total_bitmap) / total_calls + " ms");
+
+                return byteBuffer.array();
+            }
+
+            try (ByteArrayOutputStream outputStreamCompressed = new ByteArrayOutputStream()) {
+                bitmapRaw.compress(Bitmap.CompressFormat.JPEG, 80, outputStreamCompressed);
+
+                total_convert += new Date().getTime() - startTime;
+                Log.i("flutter ", "yuv_transform jpeg compression " + (new Date().getTime() - bitmapTime - startTime) + " average " + (total_convert - total_bitmap) / total_calls + " ms");
+
+                if (total_calls == 10) {
+                    FileOutputStream jpg = new FileOutputStream(new File(getCacheDir(), "rotated.jpg"));
+                    jpg.write(outputStreamCompressed.toByteArray());
+                    jpg.close();
+                    Log.i("flutter ", "yuv_transform rotated Jpeg saved to " + new File(getCacheDir(), "rotated.jpg"));
+                }
+
+                return outputStreamCompressed.toByteArray();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
--- lib/method_channelling/yuv_chanelling.dart
+++ lib/method_channelling/yuv_chanelling.dart
@@ -1,7 +1,16 @@
 import 'dart:typed_data';
 
 import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+
+/// constants from android.view.Surface
+enum Rotation {
+  ROTATION_0,
+  ROTATION_90,
+  ROTATION_180,
+  ROTATION_270,
+}
 
 class YuvChannelling {
   MethodChannel platform =
@@ -10,9 +19,10 @@ class YuvChannelling {
   ///  Transform given image to JPEG compressed through native code.
   ///
   ///  Function gets [CameraImage] in YUV format for processing and returns
-  ///  [Uint8List] of JPEG bytes.
+  ///  [Uint8List] of JPEG or BMP bytes.
+  ///  optional rotation may be 0, 90, 180, or 270
   ///
-  Future<Uint8List> yuv_transform(CameraImage image) async {
+  Future<Uint8List> yuv_transform(CameraImage image, {bool compress=true, Rotation rotation=Rotation.ROTATION_90}) async {
     List<int> strides = new Int32List(image.planes.length * 2);
     int index = 0;
     // We need to transform the image to Uint8List so that the native code could
@@ -24,13 +34,13 @@ class YuvChannelling {
       index++;
       return plane.bytes;
     }).toList();
-    Uint8List image_jpeg = await platform.invokeMethod('yuv_transform', {
+    return await platform.invokeMethod('yuv_transform', {
       'planes': planes,
       'height': image.height,
       'width': image.width,
-      'strides': strides
+      'strides': strides,
+      'compress': compress,
+      'rotation': rotation.index,
     });
-
-    return image_jpeg;
   }
 }
