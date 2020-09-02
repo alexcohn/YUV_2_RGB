1328395c7a88862f801da0ac140e002d686b13c6
alexcohn <sasha.cohn@gmail.com>
2020-09-02 18:56:32 +0300

    use a grayscale BMP for luminance instead of brendan-duncan/image.git
    reading plane[0] via list iterator (slow)

--- lib/service/image_result_processor_service.dart
+++ lib/service/image_result_processor_service.dart
@@ -1,9 +1,9 @@
 import 'dart:typed_data';
 
 import 'package:camera/camera.dart';
+
 import 'package:rxdart/subjects.dart';
 import 'package:yuvtransform/method_channelling/yuv_chanelling.dart';
-import 'package:image/image.dart';
 
 class ImageResultProcessorService  {
   YuvChannelling _yuvChannelling = YuvChannelling();
@@ -14,25 +14,35 @@ class ImageResultProcessorService  {
   Stream<Uint8List> get queue => _queue.stream;
 
   num lastTimeStamp = 0;
+  num totalTime = 0;
   num frameCount = 0;
 
   addRawImage(CameraImage cameraImage, { bool luminanceOnly = false}) async {
     frameCount += 1;
     num newTimeStamp = DateTime.now().millisecondsSinceEpoch;
     if (luminanceOnly) {
-      print("Image.fromBytes(format: luminance) ${cameraImage.width}x${cameraImage.height}: ${cameraImage.width*cameraImage.height} ${cameraImage.planes[0].bytes.length} ${cameraImage.planes[0].bytesPerRow} ");
-      Image image = Image.fromBytes(cameraImage.width, cameraImage.height, cameraImage.planes[0].bytes, format: Format.luminance, channels: Channels.rgb );
-      num beforeJpeg = DateTime.now().millisecondsSinceEpoch;
-      print("Image.fromBytes(format: ${cameraImage.format.group}) took ${beforeJpeg - newTimeStamp} ms");
-      Uint8List imgJpeg = await encodeJpg(image);
-      print("encodeJpg ${cameraImage.width}x${cameraImage.height} took ${DateTime.now().millisecondsSinceEpoch - beforeJpeg} ms");
-      _queue.sink.add(imgJpeg);
+      BMP8Header bmp;
+      int widx = 0;
+
+      var it = cameraImage.planes[0].bytes.iterator;
+      bmp = BMP8Header(cameraImage.width, cameraImage.height);
+      for (num row = 0; row < bmp.height; row++) {
+        for (num col = 0; col < bmp.width; col++) {
+          it.moveNext();
+          bmp.bd.setUint8(widx++, it.current);
+        }
+        for (num col = bmp.width; col < cameraImage.planes[0].bytesPerRow; col++) {
+          it.moveNext();
+        }
+      }
+      _queue.sink.add(bmp.list);
     }
     else {
-      Uint8List imgJpeg = await _yuvChannelling.yuv_transform(cameraImage);
-      _queue.sink.add(imgJpeg);
+      Uint8List img = await _yuvChannelling.yuv_transform(cameraImage, compress: compress, rotation: rotation);
+      _queue.sink.add(img);
     }
-    print("Job ${frameCount} took ${DateTime.now().millisecondsSinceEpoch - newTimeStamp} ms to complete" + (lastTimeStamp > 0 ? ", at ${(1000.0/(newTimeStamp-lastTimeStamp)).toStringAsFixed(1)} fps" : ""));
+    totalTime += DateTime.now().millisecondsSinceEpoch - newTimeStamp;
+    print("Job ${frameCount} took ${DateTime.now().millisecondsSinceEpoch - newTimeStamp}/${(totalTime/frameCount).toStringAsFixed(1)} ms to complete" + (lastTimeStamp > 0 ? ", at ${(1000.0/(newTimeStamp-lastTimeStamp)).toStringAsFixed(1)} fps" : ""));
     lastTimeStamp = newTimeStamp;
   }
 
@@ -40,4 +50,46 @@ class ImageResultProcessorService  {
     _queue.close();
   }
 
+}
+
+class BMP8Header {
+  int width; // NOTE: width must be multiple of 4 as no account is made for bitmap padding
+  int height;
+
+  Uint8List list;
+  ByteData bd;
+  int _totalHeaderSize;
+
+  BMP8Header(this.width, this.height) : assert(width & 3 == 0) {
+    int baseHeaderSize = 54;
+    _totalHeaderSize = baseHeaderSize + 1024; // base + color map
+    int fileLength = _totalHeaderSize + width * height * 3; // header + bitmap
+    list = new Uint8List(fileLength);
+    bd = list.buffer.asByteData(0, _totalHeaderSize);
+    bd.setUint8(0, 0x42);
+    bd.setUint8(1, 0x4d);
+    bd.setUint32(2, fileLength, Endian.little); // file length
+    bd.setUint32(10, _totalHeaderSize, Endian.little); // start of the bitmap
+    bd.setUint32(14, 40, Endian.little); // info header size
+    bd.setUint32(18, width, Endian.little);
+    bd.setInt32(22, -height, Endian.little);
+    bd.setUint16(26, 1, Endian.little); // planes
+    bd.setUint16(28, 8, Endian.little); // bpp
+    // leave everything else as zero
+
+    // color table; [0] is bright green for debugging
+    int offset = baseHeaderSize;
+    bd.setUint8(offset++, 0); // B
+    bd.setUint8(offset++, 255); // G
+    bd.setUint8(offset++, 0); // R
+    bd.setUint8(offset++, 255); // A
+    for (int rgb = 1; rgb < 256; rgb++) {
+      bd.setUint8(offset++, rgb); // B
+      bd.setUint8(offset++, rgb); // G
+      bd.setUint8(offset++, rgb); // R
+      bd.setUint8(offset++, 255); // A
+    }
+
+    bd = list.buffer.asByteData(_totalHeaderSize);
+  }
 }
